d2d0cb68d01dcc8051462aacda9783b9422b1d7d
Hendrew Hélio <hendrew.developer@gmail.com>
2020-09-24 08:08:41 -0300

    add flash message template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def flash_message_class(level)
+    case level.to_s
+    when 'notice'
+      'alert-success'
+    when 'success'
+      'alert-success'
+    when 'info'
+      'alert-info'
+    when 'error'
+      'alert-danger'
+    when 'alert'
+      'alert-danger'
+    when 'danger'
+      'alert-danger'
+    end
+  end
 end
