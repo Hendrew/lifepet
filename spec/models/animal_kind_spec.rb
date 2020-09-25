14568d23684d29538b3f005dcdb3f3e7d68bfff9
Hendrew Hélio <hendrew.developer@gmail.com>
2020-09-25 09:01:11 -0300

    animal model tests

--- spec/models/animal_kind_spec.rb
+++ spec/models/animal_kind_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe AnimalKind, type: :model do
-  it { is_expected.to validate_presence_of(:kind) }
-  it { is_expected.to validate_uniqueness_of(:kind).case_insensitive }
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:kind) }
+    it { is_expected.to validate_uniqueness_of(:kind).case_insensitive }
+  end
+
+  describe 'associations' do
+    it { is_expected.to have_many(:animals) }
+  end
 end
--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Animal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:image) }
+    it { is_expected.to validate_presence_of(:date_of_birth) }
+  end
+
+  describe 'customer validation' do
+    context 'when adoption status is true' do
+      let(:animal_kind) { create(:animal_kind) }
+      let(:animal) { build(:animal, adoption_status: true, animal_kind: animal_kind) }
+      it 'the adopter name become required' do
+        expect(animal.valid?).to eq(false)
+        expect(animal.errors[:adopter_name]).to include('não pode ficar em branco')
+      end
+    end
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:animal_kind) }
+  end
 end
